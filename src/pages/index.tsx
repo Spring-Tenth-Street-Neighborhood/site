d9b2bacf3cf29b5e0d106a99a2fbde02e6d7a3be
Jaz Singh <jazeee@gmail.com>
2025-09-06 10:27:36 -0700

    Updated content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,6 +77,39 @@ const IndexPage: React.FC<PageProps> = () => {
                       </Stack>
                     </Span>
                   </Text>
+                  <Text variant="h4" bold>
+                    Meeting Details
+                  </Text>
+                  <Text>
+                    <strong>Board Proposal:</strong> Spend $2 million of our tax
+                    payer dollars to purchase and renovate the property at 3335
+                    Spring Street into a halfway house for up to 12 residents.{" "}
+                    <ExternalLink to="https://sanmateocounty.legistar.com/LegislationDetail.aspx?ID=7649069&GUID=3A3209C8-010C-4963-BB4B-FA9DB0D34D96&Options=&Search=">
+                      View Proposal
+                    </ExternalLink>
+                  </Text>
+                  <Text>
+                    <strong>Neighborhood Concerns:</strong>
+                    <ul>
+                      <li>
+                        <ExternalLink to="https://county-taxes.net/ca-sanmateo/services/property-tax/c2FubWF0ZW8tY2E6Z3NneF9wcm9wZXJ0eV90YXg6cGFyZW50czoxMzM5MjRmYi1kNzE2LTExZWMtOTZjMy0wMDUwNTZhNDQzNDc=/bills/a0539f7f-3888-11ef-be3b-005056a4afdb#parcel">
+                          Loss of $22,602.12 in annual property tax revenue
+                        </ExternalLink>{" "}
+                        for the county and our neighborhood.
+                      </li>
+                      <li>
+                        Approximately $80,000 in loss of interest per year.
+                        (modest 4% return)
+                      </li>
+                      <li>Yearly costs for supervisory services.</li>
+                      <li>Increase in traffic and noise in the area.</li>
+                      <li>Safety and security for local residents.</li>
+                      <li>Impact on property values in the neighborhood.</li>
+                    </ul>
+                  </Text>
+                  <Text variant="h4" bold>
+                    When & Where
+                  </Text>
                   <Text>
                     Date: <strong>Tuesday, September 9, 2025 at 9 AM</strong>
                   </Text>
@@ -102,30 +135,17 @@ const IndexPage: React.FC<PageProps> = () => {
                   Board Meeting
                 </ExternalButtonLink>
                 <ExternalButtonLink
+                  component={Link}
+                  to="https://sanmateocounty.legistar.com/MeetingDetail.aspx?ID=1234924&GUID=A5CC7185-D515-4166-9667-B57F0066A392&Options=info|&Search="
+                >
+                  Board Meeting
+                </ExternalButtonLink>
+                <ExternalButtonLink
                   variant="contained"
                   to="https://smcgov.zoom.us/j/97944675398"
                 >
                   Zoom Meeting
                 </ExternalButtonLink>
-              </CardActions>
-            </Card>
-            <Card>
-              <CardContent>
-                <Stack gap={1}>
-                  <Text variant="h3" bold>
-                    Concerns around 3335 Spring Street Halfway house proposal
-                  </Text>
-                  <Text>
-                    The proposed halfway house at 3335 Spring Street has raised
-                    concerns among local residents regarding its potential
-                    impact on the neighborhood.
-                  </Text>
-                </Stack>
-              </CardContent>
-              <CardActions>
-                <InternalLink to="/concerns-around-3335-spring-street-halfway-house-proposal">
-                  Read More
-                </InternalLink>
               </CardActions>
             </Card>
           </Stack>
