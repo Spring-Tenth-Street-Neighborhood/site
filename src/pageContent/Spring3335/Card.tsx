667768360a13d444c9c27d36d1ce1ea3a0e560ef
Jaz Singh <jazeee@gmail.com>
2025-09-06 16:46:53 -0700

    Split and translate

---
+++ src/pageContent/Spring3335/Card.tsx
@@ -0,0 +1,220 @@
+import InfoIcon from "@mui/icons-material/Info";
+import { type PageProps } from "gatsby";
+import { Layout } from "../../lib/Layout";
+import { Title } from "../../lib/Typography/Title";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Divider,
+  Stack,
+} from "@mui/material";
+import { Text } from "../../lib/Typography/Text";
+import { ExternalLink, InternalLink } from "../../lib/Links";
+import Spring3335FrontImage from "../../images/spring-3335-front-33.jpg";
+import { Span } from "../../lib/Span";
+import { Spring3335Links } from "../../pageContent/Spring3335/Links";
+import { useLanguageContext } from "../../lib/Language/context";
+import { Spring3335CardListItems } from "./CardListItems";
+
+const ContentByLanguage = {
+  en: {
+    title: "3335 Spring Street Halfway house Board Meeting",
+    helloNeighbors: `Hello neighbors!`,
+    theCountyGovernment: `The county government is trying to fast track this project into our neighborhood without allowing us residents enough time and information to discuss it. This will affect our neighborhood for decades.`,
+    actionNeeded: `Action Needed:`,
+    attendMeeting: `Attend the upcoming board meeting to voice your concerns and stay informed about this important issue.`,
+    info1: `The last meeting required less than 30 minutes of time.`,
+    info2: `It was easy to provide public comment in `,
+    info3: `Zoom`,
+    info4: `Simply raise your hand.`,
+    whenWhere: "When & Where",
+    date: (
+      <>
+        Date: <strong>Tuesday, September 9, 2025 at 9 AM</strong>
+      </>
+    ),
+    zoomLink: "Zoom Link:",
+    location: (
+      <>
+        Location:{" "}
+        <strong>500 County Center Chambers, 1st Fl. Redwood City, CA</strong>
+      </>
+    ),
+    meetingDetails: "Meeting Details",
+    boardProposal: (
+      <>
+        <strong>Board Proposal:</strong> Spend{" "}
+        <Text component="span" color="warning" bold>
+          $2 million
+        </Text>{" "}
+        of our tax payer dollars to purchase and renovate the property at 3335
+        Spring Street into a halfway house for up to 12 residents.
+      </>
+    ),
+    proposalLink: "Proposal",
+    meetingLink: "Meeting",
+    flyerLink: "Flyer",
+    neighborhoodConcerns: "Neighborhood Concerns",
+    theNeighborhoodAround:
+      "The neighborhood around 3335 Spring Street are very concerned regarding the proposed halfway house.",
+    petitionLink: "Petition with at least 123 signatures",
+    lossOfPropertyTax:
+      "Loss of $22,602.12 in annual property tax revenue for the county and our neighborhood.",
+    approximatelyLoss:
+      "Approximately $80,000 in loss of interest per year. (modest 4% return)",
+    yearlyCostsSupervisory: "Yearly costs for supervisory 24 hour services.",
+    yearlyCostsMaintenance: "Yearly costs for maintenance and repair.",
+    priorExampleAt:
+      "Prior example at 3787, 3787 Hoover Street and 1143 Marsh Road",
+    plansToPlace: "Plans to place 12 residents in a single-family home.",
+    initialProposal:
+      'Initial proposal: "begin with no more than six residents at a time during the initial six months of operation".',
+    messagingIndicated: "Messaging indicated 12 men and women over time.",
+  },
+  es: {
+    title: "Reunión de la Junta de 3335 Spring Street Casa de Medio Camino",
+    helloNeighbors: `¡Hola vecinos!`,
+    theCountyGovernment: `El gobierno del condado está tratando de acelerar este proyecto en nuestro vecindario sin permitir que nosotros, los residentes, tengamos suficiente tiempo e información para discutirlo. Esto afectará a nuestro vecindario durante décadas.`,
+    actionNeeded: `Acción Necesaria:`,
+    attendMeeting: `Asista a la próxima reunión de la junta para expresar sus preocupaciones y mantenerse informado sobre este importante tema.`,
+    info1: `La última reunión requirió menos de 30 minutos de tiempo.`,
+    info2: `Fue fácil proporcionar comentarios públicos en `,
+    info3: `Zoom`,
+    info4: `Simplemente levante la mano.`,
+    whenWhere: "Cuándo y Dónde",
+    date: (
+      <>
+        Fecha: <strong>Martes, 9 de septiembre de 2025 a las 9 AM</strong>
+      </>
+    ),
+    zoomLink: "Enlace de Zoom:",
+    location: (
+      <>
+        Ubicación:{" "}
+        <strong>500 County Center Chambers, 1st Fl. Redwood City, CA</strong>
+      </>
+    ),
+    meetingDetails: "Detalles de la Reunión",
+    boardProposal: (
+      <>
+        <strong>Propuesta de la Junta:</strong> Gastar{" "}
+        <Text component="span" color="warning" bold>
+          $2 millones
+        </Text>{" "}
+        de nuestros dólares de impuestos para comprar y renovar la propiedad en
+        3335 Spring Street y convertirla en una casa de medio camino para hasta
+        12 residentes.
+      </>
+    ),
+    proposalLink: "Propuesta",
+    meetingLink: "Reunión",
+    flyerLink: "Volante",
+    neighborhoodConcerns: "Preocupaciones del Vecindario",
+    theNeighborhoodAround:
+      "El vecindario alrededor de 3335 Spring Street está muy preocupado por la propuesta de una casa de medio camino.",
+    petitionLink: "Petición con al menos 123 firmas",
+    lossOfPropertyTax:
+      "Pérdida de $22,602.12 en ingresos anuales por impuestos a la propiedad para el condado y nuestro vecindario.",
+    approximatelyLoss:
+      "Aproximadamente $80,000 en pérdida de intereses por año. (modesto retorno del 4%)",
+    yearlyCostsSupervisory:
+      "Costos anuales por servicios de supervisión las 24 horas.",
+    yearlyCostsMaintenance: "Costos anuales de mantenimiento y reparación.",
+    priorExampleAt:
+      "Ejemplo previo en 3787, 3787 Hoover Street y 1143 Marsh Road",
+    plansToPlace:
+      "Planes para colocar a 12 residentes en una casa unifamiliar.",
+    initialProposal:
+      'Propuesta inicial: "comenzar con no más de seis residentes a la vez durante los primeros seis meses de operación".',
+    messagingIndicated: "El mensaje indicó 12 hombres y mujeres con el tiempo.",
+  },
+};
+
+export function Spring3335Card() {
+  const { language } = useLanguageContext();
+  const content = ContentByLanguage[language];
+  return (
+    <Card>
+      <CardContent>
+        <Stack gap={1}>
+          <Text variant="h2" bold>
+            {content.title}
+          </Text>
+          <Text>
+            <strong>{content.helloNeighbors}</strong>
+            <br />
+            {content.theCountyGovernment}
+          </Text>
+          <CardMedia
+            component="img"
+            src={Spring3335FrontImage}
+            alt="3335 Spring Street Halfway House"
+          />
+          <Text component="span" color="textSecondary" bold>
+            {content.actionNeeded}
+          </Text>
+          <Text>{content.attendMeeting}</Text>
+          <Text>
+            <Span alignContent={"center"} display="flex" gap={1}>
+              <InfoIcon color="primary" />
+              <Stack display="inline-flex">
+                {content.info1}
+                <Span>
+                  {content.info2}{" "}
+                  <ExternalLink to="https://smcgov.zoom.us/j/97944675398">
+                    {content.info3}
+                  </ExternalLink>
+                  .
+                </Span>
+                <Span>{content.info4}</Span>
+              </Stack>
+            </Span>
+          </Text>
+          <Text variant="h3" bold>
+            {content.whenWhere}
+          </Text>
+          <Text>{content.date}</Text>
+          <Text>
+            {content.zoomLink}{" "}
+            <ExternalLink to="https://smcgov.zoom.us/j/97944675398">
+              https://smcgov.zoom.us/j/97944675398
+            </ExternalLink>
+          </Text>
+          <Text>{content.location}</Text>
+          <Text variant="h3" bold>
+            {content.meetingDetails}
+          </Text>
+          <Text>
+            {content.boardProposal}{" "}
+            <ExternalLink to="https://www.smcgov.org/ceo/south-county-youth-housing">
+              {content.proposalLink}
+            </ExternalLink>{" "}
+            <ExternalLink to="https://sanmateocounty.legistar.com/LegislationDetail.aspx?ID=7649069&GUID=3A3209C8-010C-4963-BB4B-FA9DB0D34D96&Options=&Search=">
+              {content.meetingLink}
+            </ExternalLink>{" "}
+            <InternalLink to="/spring-st-3335">
+              {content.flyerLink}
+            </InternalLink>
+          </Text>
+          <Text variant="h3" bold component="span" display="block">
+            {content.neighborhoodConcerns}
+          </Text>
+          <Text>
+            {content.theNeighborhoodAround}
+            <ExternalLink to="https://chng.it/SYb2wGrqZC">
+              {content.petitionLink}
+            </ExternalLink>
+            <Spring3335CardListItems />
+          </Text>
+        </Stack>
+        <Divider />
+      </CardContent>
+      <CardActions>
+        <Spring3335Links />
+      </CardActions>
+    </Card>
+  );
+}
---
+++ src/pageContent/Spring3335/CardListItems.tsx
@@ -0,0 +1,174 @@
+import InfoIcon from "@mui/icons-material/Info";
+import { type PageProps } from "gatsby";
+import { Layout } from "../../lib/Layout";
+import { Title } from "../../lib/Typography/Title";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Divider,
+  Stack,
+} from "@mui/material";
+import { Text } from "../../lib/Typography/Text";
+import { ExternalLink, InternalLink } from "../../lib/Links";
+import Spring3335FrontImage from "../../images/spring-3335-front-33.jpg";
+import { Span } from "../../lib/Span";
+import { Spring3335Links } from "./Links";
+import { useLanguageContext } from "../../lib/Language/context";
+
+const ContentByLanguage = {
+  en: {
+    lossOfPropertyTax:
+      "Loss of $22,602.12 in annual property tax revenue for the county and our neighborhood.",
+    approximatelyLoss:
+      "Approximately $80,000 in loss of interest per year. (modest 4% return)",
+    yearlyCostsSupervisory: "Yearly costs for supervisory 24 hour services.",
+    yearlyCostsMaintenance: "Yearly costs for maintenance and repair.",
+    priorExampleAt:
+      "Prior example at 3787, 3787 Hoover Street and 1143 Marsh Road",
+    plansToPlace: "Plans to place 12 residents in a single-family home.",
+    initialProposal:
+      'Initial proposal: "begin with no more than six residents at a time during the initial six months of operation".',
+    messagingIndicated: "Messaging indicated 12 men and women over time.",
+    precedence: "Precedence",
+    serviceLeague: "Service League of San Mateo",
+    operation1: (
+      <>
+        "operate five, six-bed homes{" "}
+        <Text component="span" color="warning" bold>
+          (2 for women* and 3 for men)
+        </Text>{" "}
+        in residential settings throughout Redwood City."
+      </>
+    ),
+    operation2: (
+      <>
+        In other words, up to{" "}
+        <Text component="span" color="warning" bold>
+          18
+        </Text>{" "}
+        people could be housed at once.
+      </>
+    ),
+    operation3: `Our neighborhood is not equipped to handle 12-18 residents, or their
+            associated traffic, sewage service, parking, and other
+            infrastructure needs.`,
+    legalRecourse: "Legal recourse",
+    zoningLaws:
+      "Residential neighborhoods can challenge the placement of such facilities through local zoning laws and regulations.",
+    legalPrecedence:
+      "Legal Precedence: https://supreme.justia.com/cases/federal/us/416/1/",
+    supremeCourt:
+      "The U.S. Supreme Court allowed the legislature to draw distinctions that limit the number of unrelated people who can constitute a family.",
+    increaseTraffic: "Increase in traffic and noise in the area.",
+    safetySecurity: "Safety and security for local residents.",
+    impactPropertyValues: "Impact on property values in the neighborhood.",
+  },
+  es: {
+    lossOfPropertyTax:
+      "Pérdida de $22,602.12 en ingresos anuales por impuestos a la propiedad para el condado y nuestro vecindario.",
+    approximatelyLoss:
+      "Aproximadamente $80,000 en pérdida de intereses por año. (modesto retorno del 4%)",
+    yearlyCostsSupervisory:
+      "Costos anuales por servicios de supervisión las 24 horas.",
+    yearlyCostsMaintenance: "Costos anuales de mantenimiento y reparación.",
+    priorExampleAt:
+      "Ejemplo previo en 3787, 3787 Hoover Street y 1143 Marsh Road",
+    plansToPlace:
+      "Planes para colocar a 12 residentes en una casa unifamiliar.",
+    initialProposal:
+      'Propuesta inicial: "comenzar con no más de seis residentes a la vez durante los primeros seis meses de operación".',
+    messagingIndicated: "El mensaje indicó 12 hombres y mujeres con el tiempo.",
+    precedence: "Precedencia",
+    serviceLeague: "Service League de San Mateo",
+    operation1: (
+      <>
+        "operar cinco casas de seis camas{" "}
+        <Text component="span" color="warning" bold>
+          (2 para mujeres* y 3 para hombres)
+        </Text>{" "}
+        en entornos residenciales en todo Redwood City."
+      </>
+    ),
+    operation2: (
+      <>
+        En otras palabras, hasta{" "}
+        <Text component="span" color="warning" bold>
+          18
+        </Text>{" "}
+        personas podrían ser alojadas a la vez.
+      </>
+    ),
+    operation3: `Nuestro vecindario no está equipado para manejar de 12 a 18 residentes, o sus
+            necesidades asociadas de tráfico, servicio de alcantarillado, estacionamiento y otra
+            infraestructura.`,
+    legalRecourse: "Recurso legal",
+    zoningLaws:
+      "Los vecindarios residenciales pueden impugnar la colocación de tales instalaciones a través de las leyes y regulaciones de zonificación locales.",
+    legalPrecedence:
+      "Precedencia legal: https://supreme.justia.com/cases/federal/us/416/1/",
+    supremeCourt:
+      "La Corte Suprema de los EE. UU. permitió que la legislatura hiciera distinciones que limitan el número de personas no relacionadas que pueden constituir una familia.",
+    increaseTraffic: "Aumento del tráfico y ruido en el área.",
+    safetySecurity: "Seguridad para los residentes locales.",
+    impactPropertyValues:
+      "Impacto en los valores de las propiedades en el vecindario.",
+  },
+};
+
+export function Spring3335CardListItems() {
+  const { language } = useLanguageContext();
+  const content = ContentByLanguage[language];
+  return (
+    <ul>
+      <li>
+        <ExternalLink to="https://county-taxes.net/ca-sanmateo/services/property-tax/c2FubWF0ZW8tY2E6Z3NneF9wcm9wZXJ0eV90YXg6cGFyZW50czoxMzM5MjRmYi1kNzE2LTExZWMtOTZjMy0wMDUwNTZhNDQzNDc=/bills/a0539f7f-3888-11ef-be3b-005056a4afdb#parcel">
+          {content.lossOfPropertyTax}
+        </ExternalLink>
+      </li>
+      <li>{content.approximatelyLoss}</li>
+      <li>{content.yearlyCostsSupervisory}</li>
+      <li>
+        {content.yearlyCostsSupervisory}{" "}
+        <ExternalLink to="https://www.smcgov.org/ceo/repairs-transitional-housing-service-league-san-mateo-county">
+          {content.priorExampleAt}
+        </ExternalLink>
+      </li>
+      <li>
+        {content.plansToPlace}
+        <ul>
+          <li>{content.initialProposal}</li>
+          <li>{content.messagingIndicated}</li>
+        </ul>
+      </li>
+      <li>
+        {content.precedence}:{" "}
+        <ExternalLink to="https://serviceleague.org/get-help/transitional-housing.html#:~:text=We%20operate%20five%2C%20six%2Dbed%20homes%20(2%20for%20women*%20and%203%20for%20men)%20in%20residential%20settings%C2%A0throughout%20Redwood%20City.">
+          {content.serviceLeague}
+        </ExternalLink>
+        <ul>
+          <li>{content.operation1}</li>
+          <li>{content.operation2}</li>
+          <li>{content.operation3}</li>
+        </ul>
+      </li>
+      <li>
+        {content.legalRecourse}:
+        <ul>
+          <li>{content.zoningLaws}</li>
+          <li>
+            <ExternalLink to="https://supreme.justia.com/cases/federal/us/416/1/">
+              {content.legalPrecedence}
+            </ExternalLink>
+          </li>
+          <li>{content.supremeCourt}</li>
+        </ul>
+      </li>
+      <li>{content.increaseTraffic}</li>
+      <li>{content.safetySecurity}</li>
+      <li>{content.impactPropertyValues}</li>
+    </ul>
+  );
+}
